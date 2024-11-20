e8e75d743a2283e663ffb3ea08a0ddfe52f485a3
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-11-20 11:55:34 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,50 @@
-
 #!/usr/bin/node
-
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+let movieIdentification = '';
+try {
+  movieIdentification = process.argv[2];
+  if (movieIdentification === undefined) {
+    throw new Error('Exceeded command line argument length');
+  }
+} catch (error) {
+  console.log(`Usage: ${process.argv[1]} movieIdentification`);
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieIdentification}/`;
+
+// Make request
+let movie = '';
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`[${response.statusCode}]: ${response}`);
+  } else {
+    movie = JSON.parse(body);
+    displayCharacters(movie);
+  }
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+
+/**
+ * name - displayCharacters
+ * @movie: the movie object to log
+ */
+function displayCharacters (movie) {
+  if (movie === undefined) {
+    return;
+  }
+  movie.characters.forEach((url) => {
+    request.get((url), (error, response, body) => {
+      if (error) {
+        console.error('Error', error);
+      } else if (response.statusCode !== 200) {
+        console.error(`[${response.statusCode}]: ${response}`);
+      } else {
+        const person = JSON.parse(body);
+        console.log(person.name);
+      }
+    });
   });
-};
+}
